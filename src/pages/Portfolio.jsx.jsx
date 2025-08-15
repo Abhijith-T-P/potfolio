e785438bd76f0152a091f9af30504fd51b2265f6
Abhijith <abhijithtp2003@gmail.com>
2025-08-15 21:10:15 +0530

    live image

--- src/pages/Portfolio.jsx.jsx
+++ src/pages/Portfolio.jsx.jsx
@@ -254,7 +254,7 @@ const Portfolio = () => {
             <div className="about-image">
               <div className="image-container">
                 <img
-                  src={profileImg}
+                  src="https://media.licdn.com/dms/image/v2/D5603AQEQIMv_Q4M-uQ/profile-displayphoto-crop_800_800/B56ZiuXQK7HUAQ-/0/1755271988689?e=1758153600&v=beta&t=177XrNCWIjozT8burXeEf3WhETV0JmqNgAOcoDys-yU"
                   alt="Profile"
                 />
               </div>
